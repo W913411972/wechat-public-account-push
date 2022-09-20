5cc51eea06aee46ef5d447c130ce53316f86fb7b
Strakkys <44767749+W913411972@users.noreply.github.com>
2022-09-20 16:48:19 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -28,13 +28,13 @@ export const config = {
 
     /** 每日N句 */
     // 金山每日一句
-    CIBA: true,
+    CIBA: false,
     // 每日一言
-    oneTalk: true,
+    oneTalk: false,
     // 土味情话(彩虹屁)
     earthyLoveWords: true,
     // 朋友圈文案
-    momentCopyrighting: false,
+    momentCopyrighting: true,
     // 毒鸡汤
     poisonChickenSoup: false,
     // 古诗古文
